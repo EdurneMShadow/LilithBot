fad46c3c270a7d8d031624da25b9b7392fe40099
EdurneMShadow <edurnecp22@gmail.com>
2017-12-19 23:58:37 +0100

    Creado un filtro para el modo dictadura, que ya está terminado. Manda un mensaje al grupo y además por privado a cada uno de los miembros el pdf de la constitución y las fotos de la bandera y del gran lider.

--- lilith_bot.py
+++ lilith_bot.py
@@ -7,11 +7,17 @@ logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s
 logger = logging.getLogger(__name__)
 
 id_grupo = '-310490484'
+id_miembros = [169092, 7730198, 39226004, 72263992, 172732847, 173507959, 184637992, 186522250, 188858869,
+                197978154, 350932157]
+constitucion = open('70-Idioms.pdf', 'rb')
+#bandera = open('tests/test.png', 'rb')
+#foto = open('tests/test.png', 'rb')
 filter_dictadura = filtros.FilterDictadura()
 
 def main():
     updater = Updater(token='462365078:AAEDXX4Wj1_N_Li7Yug1fvXZhvQvuMfLlog')
     dispatcher = updater.dispatcher
+
     dictadura_handler = MessageHandler(filter_dictadura, modo_dictadura)
     dispatcher.add_handler(dictadura_handler)
 
@@ -32,13 +38,17 @@ def modo_dictadura(bot, update):
     - Subida de los impuestos. \n\
     Para deshacer este modo, TODO integrante del grupo deberá enviar una foto con el filtro del gato de Instagram.')
 
-
     user = update.message.from_user
     logger.info('Nombre del usuario: %s , ID del usuario: %s, Mensaje: %s, ID del grupo: %s',
                 user.first_name, user.id, update.message.text, update.message.chat)
+
     bot.send_message(chat_id= id_grupo, text=bando)
 
-
+    for i in id_miembros:
+        bot.send_message(chat_id = i , text = 'HOLA EDURNE')
+        bot.sendDocument(chat_id = i , document = constitucion)
+        bot.send_photo(chat_id = i, photo = bandera)
+        bot.send_photo(chat_id = i, photo = foto)
 
 if __name__ == '__main__':
     main()
